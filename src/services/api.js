5f9b32ae1e399e01e8668da01517cb17e61d0fc8
dhritij27 <PES1UG23CS185@pesu.pes.edu>
2026-02-03 11:13:46 -0500

    Add API functions to fetch users and user by ID

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+export const fetchUsers = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  if (!response.ok) throw new Error('Failed to fetch users')
+  return response.json()
+}
+
+export const fetchUserById = async (id) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  )
+  if (!response.ok) throw new Error('Failed to fetch user')
+  return response.json()
+}
